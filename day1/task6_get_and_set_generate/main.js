f25c9ecec2ae8e32dbfe4c402ac78a6c6f028ec9
ahmedelayk <ahmedelayek7@gmail.com>
2023-04-07 13:48:22 +0200

    d1t6

--- day1/task6_get_and_set_generate/main.js
+++ day1/task6_get_and_set_generate/main.js
@@ -6,24 +6,49 @@ var user = {
   address: 'aghour',
   displayInfo: function(){
     return 'id:: '+this.id+', name:: '+this.name+', age:: '+this.age+', address:: '+this.address
+  },
+  getSetGen: function(){
+    var self = this
+    // console.log('out',self)
+    for(key in self){
+      // console.log('inner',self);
+      (function(key){
+        // console.log(self);
+        if(typeof self[key] !== 'function'){
+          self['set'+key] = function(val){
+            self[key] = val
+          }
+          self['get'+key] = function(){
+            return self[key]
+          }
+        }
+      })(key)
+    }
   }
 }
+var obj = {
+  id:700,
+  location: "sd101",
+}
+user.getSetGen()
+user.getSetGen.apply(obj)
 
-function getSetGen(obj){
-  for(key in obj){
-    (function(key){
-      if(typeof obj[key] !== 'function'){
-        obj['set'+key] = function(val){
-          this[key] = val
-        }
-        obj['get'+key] = function(){
-          return this[key]
-        }
-      }
-    })(key)
-  }
-}
-getSetGen(user)
+// function getSetGen(obj){
+//   for(key in obj){
+//     (function(key){
+//       if(typeof obj[key] !== 'function'){
+//         obj['set'+key] = function(val){
+//           this[key] = val
+//         }
+//         obj['get'+key] = function(){
+//           return this[key]
+//         }
+//       }
+//     })(key)
+//   }
+// }
+// getSetGen(user)
+
 console.log(user.displayInfo())
 user.setid(7)
 user.setname('mohamed')
